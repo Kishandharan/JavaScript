f9434a364769b6c3a5fd9eff1aef6aa13033e22b
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-04-19 16:22:39 +0400

    Changes

--- 3.js
+++ 3.js
@@ -1,27 +1,11 @@
-// Calculator
-var input1 = 0;
-var operat = "";
-var input2 = 0;
+function func1(cb){
+	console.log("foo")
+	cb()
+	console.log("bar")
+}
 
-require("readline").createInterface({
-	input: process.stdin,
-	output: process.stdout
-}).question("Number1: ", (usrinp) => {
-	input1 = Number(usrinp);
-})
+function callback(){
+	console.log("Executing callback")
+}
 
-require("readline").createInterface({
-	input: process.stdin,
-	output: process.stdout
-}).question("Number2: ", (usrinp) => {
-	input2 = Number(usrinp);
-})
-
-
-require("readline").createInterface({
-	input: process.stdin,
-	output: process.stdout
-}).question("Operat: ", (usrinp) => {
-	operat = usrinp;
-})
-
+func1(callback)
